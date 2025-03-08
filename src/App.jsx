30542aaf2815fe9a4e56c4940f321b91c1591270
EniolaCodes <adebimpeniola@gmail.com>
2025-03-08 20:32:46 +0100

    Initial: Boilerplate setup and git initialization

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-500 text-center text-2xl">
+        Hello CrestTech Team!
+      </h1>
+    </>
+  );
+}
+
+export default App;
