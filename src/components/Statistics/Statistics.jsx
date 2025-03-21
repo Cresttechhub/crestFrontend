cab3e3f7ea02500fc354271d6c85744a1c2cb65d
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2025-03-21 22:49:38 +0100

    Eniola (#3)
    * feat(homepage): Restructure layout to align with updated design specifications
    
    * feat(hero_component): implement animated text loop, input field and floating icons
    
    * fix(hero_component): correctly align profile images
    
    * feat(statistics_component): Implement animated stats section that triggers on viewport entry
    
    * feat(statistics_component): Implement animated stat section that triggers on viewport entry

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,105 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Statistics = () => {
+  const [stat1, setStat1] = useState(10);
+  const [stat2, setStat2] = useState(50);
+  const [stat3, setStat3] = useState(30);
+  const [ref, inView] = useInView();
 
-  return <div className="px-16 py-6">I am Statistics</div>;
+  useEffect(() => {
+    if (inView) {
+      let count = 0;
+      const interval = setInterval(() => {
+        count++;
+        setStat1((prev) => prev + 10);
+        setStat2((prev) => prev + 50);
+        setStat3((prev) => prev + 30);
 
+        if (count === 3) {
+          clearInterval(interval);
+          setStat1(40);
+          setStat2(200);
+          setStat3(95);
+        }
+      }, 1000);
+
+      return () => {
+        clearInterval(interval);
+        setStat1(10);
+        setStat2(50);
+        setStat3(30);
+      };
+    } else {
+      setStat1(10);
+      setStat2(50);
+      setStat3(30);
+    }
+  }, [inView]);
+
+  const statVariants = {
+    initial: { y: "100%", opacity: 0 },
+    animate: { y: "0%", opacity: 1 },
+    exit: { y: "-100%", opacity: 0 },
+  };
+  return (
+    <div className="px-16 py-36 " ref={ref}>
+      <div className="bg-[#1E1E1E] text-white rounded-[25px] container mx-auto flex justify-around items-center pt-20 pb-20">
+        <div className="text-center">
+          <AnimatePresence initial={false} mode="wait">
+            <motion.h2
+              key={stat1}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              variants={statVariants}
+              className="text-[50px] font-medium"
+            >
+              {stat1} +
+            </motion.h2>
+          </AnimatePresence>
+          <p className="text-[16px] mt-2">Expert-Led Courses</p>
+        </div>
+
+        <div className="h-40 border-r border-white" />
+        <div className="text-center">
+          <AnimatePresence initial={false} mode="wait">
+            <motion.h2
+              key={stat2}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              variants={statVariants}
+              className="text-[50px] font-medium"
+            >
+              {stat2} +
+            </motion.h2>
+          </AnimatePresence>
+          <p className="text-[16px] mt-2">
+            Hours of Live & Self-Paced Learning
+          </p>
+        </div>
+
+        <div className="h-40 border-r border-white" />
+        <div className="text-center">
+          <AnimatePresence initial={false} mode="wait">
+            <motion.h2
+              key={stat3}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              variants={statVariants}
+              className="text-[50px] font-medium"
+            >
+              {stat3} %
+            </motion.h2>
+          </AnimatePresence>
+          <p className="text-[16px] mt-2">Student Satisfaction Rate</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
