ca0afee0b74e28640f56083f66ae0763fe5b213b
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2025-03-30 06:24:41 +0100

    Development (#13)
    * feat(homepage): Restructure layout to align with updated design specifications
    
    * feat(hero_component): implement animated text loop, input field and floating icons
    
    * fix(hero_component): correctly align profile images
    
    * feat(statistics_component): Implement animated stats section that triggers on viewport entry
    
    * feat(statistics_component): Implement animated stat section that triggers on viewport entry
    
    * feat(About_component): add the about section with navigation to the about page
    
    * feat: Add About component with in-view text animation and menu hover effect
    
    * fix(Statistics_component) :Stepwise Count-Up with Smooth Upward Animation
    
    * fix: resolve merge conflict in About component
    
    * Add program component with pricing and signup link
    
    * feat(auth): Add authentication pages for enrollment, signup, login, and password management
    
    * feat(Choice_component): add choice section with icons, title and description
    
    * feat(about_page): create about page, including leadership team, core values, and impact sections
    
    * fix(modal): Adjust overlay opacity to allow background content to be more
    
    * feat(program_page): Create hero section with text and image layout
    
    * fix(program_page): Adjust thinGreen1 & thinGreen2 positioning to ensure it stays behind program_hero
    
    * feat(mentorship_page_: add mentorship page with hero and mentors section
    
    * Updated header, footer and partners section (#9)
    
    * feat: create FAQs page with framer motion for smooth animations
    
    * feat: add contact page with form submission
    
    * feat: add enroll page with form validation and dynamic image carousel
    
    * feat(Hero): Make Hero component responsive
    
    ---------
    
    Co-authored-by: Gbadamosi Sofiyat <sofiyatgbadamosi@gmail.com>

--- src/pages/auth/enroll.jsx
+++ src/pages/auth/enroll.jsx
@@ -1,7 +1,204 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import image1 from "../../images/enroll_img.svg";
+import image2 from "../../images/enroll_img1.svg";
+import { IoArrowBackOutline, IoChevronDownOutline } from "react-icons/io5";
 
-const enroll = () => {
-  return <div>enroll</div>;
+const images = [image1, image2];
+
+const programs = [
+  "4-Week Bootcamp",
+  "3-Month Masterclass",
+  "UI/UX Track",
+  "Software Development Track",
+];
+
+const Enroll = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    program: "",
+  });
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prev) => (prev + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const validateForm = () => {
+    let newErrors = {};
+    if (!formData.name) newErrors.name = "Full Name is required";
+    if (!formData.email) {
+      newErrors.email = "Email is required";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+      newErrors.email = "Invalid email format";
+    }
+    if (!formData.phone) {
+      newErrors.phone = "Phone Number is required";
+    } else if (!/^\d{10}$/.test(formData.phone)) {
+      newErrors.phone = "Invalid phone number";
+    }
+
+    if (!formData.program) {
+      newErrors.program = "Please select a program";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+
+    setIsSubmitting(true);
+
+    setTimeout(() => {
+      setIsSubmitting(false);
+      setIsSubmitted(true);
+
+      // Reset the form after a few seconds
+      setTimeout(() => {
+        setIsSubmitted(false);
+        setFormData({ name: "", email: "", phone: "", program: "" });
+      }, 5000); // Resets after 5 seconds
+    }, 2000);
+  };
+
+  return (
+    <div className="mt-32 md:mt-56 md:px-16 md:py-6 p-8 flex flex-col">
+      <Link
+        to="/"
+        className="flex font-bold items-center space-x-2 text-[#1E1E1E] mb-8"
+      >
+        <IoArrowBackOutline />
+        <h1 className="text-[16px]">Back</h1>
+      </Link>
+      <div className="flex flex-col md:flex-row md:justify-between md:space-x-8">
+        <div className="hidden md:flex w-full">
+          <img
+            src={images[currentImage]}
+            alt="Enrollment image"
+            className="w-full h-[720px] rounded-tl-[25px] rounded-bl-[25px] object-cover transition-opacity duration-700"
+          />
+        </div>
+        {/* form */}
+        <form
+          onSubmit={handleSubmit}
+          className="space-y-6 mt-8 md:mt-0 md:py-8 w-full h-full"
+        >
+          <h2 className="text-[28px] font-bold text-[#1E1E1E] -mt-8 md:mt-0">
+            Enroll Now
+          </h2>
+          <div className="">
+            <label htmlFor="" className="">
+              Full Name
+            </label>
+            <input
+              type="text"
+              placeholder="Enter your full name"
+              value={formData.name}
+              onChange={(e) =>
+                setFormData({ ...formData, name: e.target.value })
+              }
+              className="mt-4 w-full p-3 border border-[#1E1E1E] rounded-[15px] focus:outline-none focus:ring focus:ring-[#1E1E1E]"
+            />
+            {errors.name && (
+              <p className="text-red-500 text-sm">{errors.name}</p>
+            )}
+          </div>
+          <div className="">
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+              className="mt-4 w-full p-3 border border-[#1E1E1E] rounded-[15px] focus:outline-none focus:ring focus:ring-[#1E1E1E]"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email}</p>
+            )}
+          </div>
+          <div className="">
+            <label htmlFor="">Phone Number</label>
+            <input
+              type="number"
+              placeholder="Enter your phone number"
+              value={formData.phone}
+              onChange={(e) =>
+                setFormData({ ...formData, phone: e.target.value })
+              }
+              className="mt-4 w-full p-3 border border-[#1E1E1E] rounded-[15px] focus:outline-none focus:ring focus:ring-[#1E1E1E]"
+            />
+            {errors.phone && (
+              <p className="text-red-500 text-sm">{errors.phone}</p>
+            )}
+          </div>
+          <div className="relative">
+            <label className="block mb-2">Programs</label>
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="w-full p-3 border border-[#1E1E1E] rounded-[15px] flex justify-between items-center focus:outline-none focus:ring focus:ring-[#1E1E1E]"
+            >
+              {formData.program || "Select a program"}
+              <IoChevronDownOutline className="text-[#1E1E1E]" />
+            </button>
+
+            {isOpen && (
+              <ul className="absolute w-full bg-white rounded-[15px] p-8 mt-4 shadow-lg z-10">
+                {programs.map((program) => (
+                  <li
+                    key={program}
+                    onClick={() => {
+                      setFormData({ ...formData, program });
+                      setIsOpen(false);
+                    }}
+                    className="px-2 py-3 hover:bg-[#009E65] hover:text-[white] hover:rounded-[15px] cursor-pointer"
+                  >
+                    {program}
+                  </li>
+                ))}
+              </ul>
+            )}
+
+            {errors.program && (
+              <p className="text-red-500 text-sm">{errors.program}</p>
+            )}
+          </div>
+
+          <button
+            disabled={isSubmitting || isSubmitted}
+            type="submit"
+            className={`w-full mt-8 text-white p-3 rounded-[15px] text-[16px] transition ${
+              isSubmitted ? "bg-[#1cf3a4]" : "bg-[#009E65]"
+            }`}
+          >
+            {isSubmitting ? (
+              <span className="animate-spin inline-block w-4 h-4 border-2 border-white border-t-transparent rounded-full mr-2"></span>
+            ) : null}
+            {isSubmitting
+              ? "Submitting..."
+              : isSubmitted
+              ? "Application submitted successfully we'll contact you soon!"
+              : "Submit Application"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
-export default enroll;
+export default Enroll;
