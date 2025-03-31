512be6b2ac1111a2f12cff933d0744850b183203
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2025-03-31 04:49:06 +0100

    Development (#21)
    * feat(homepage): Restructure layout to align with updated design specifications
    
    * feat(hero_component): implement animated text loop, input field and floating icons
    
    * fix(hero_component): correctly align profile images
    
    * feat(statistics_component): Implement animated stats section that triggers on viewport entry
    
    * feat(statistics_component): Implement animated stat section that triggers on viewport entry
    
    * feat(About_component): add the about section with navigation to the about page
    
    * feat: Add About component with in-view text animation and menu hover effect
    
    * fix(Statistics_component) :Stepwise Count-Up with Smooth Upward Animation
    
    * fix: resolve merge conflict in About component
    
    * Add program component with pricing and signup link
    
    * feat(auth): Add authentication pages for enrollment, signup, login, and password management
    
    * feat(Choice_component): add choice section with icons, title and description
    
    * feat(about_page): create about page, including leadership team, core values, and impact sections
    
    * fix(modal): Adjust overlay opacity to allow background content to be more
    
    * feat(program_page): Create hero section with text and image layout
    
    * fix(program_page): Adjust thinGreen1 & thinGreen2 positioning to ensure it stays behind program_hero
    
    * feat(mentorship_page_: add mentorship page with hero and mentors section
    
    * Updated header, footer and partners section (#9)
    
    * feat: create FAQs page with framer motion for smooth animations
    
    * feat: add contact page with form submission
    
    * feat: add enroll page with form validation and dynamic image carousel
    
    * feat(Hero): Make Hero component responsive
    
    * feat(About & Statistics): Make About & Statistics component responsive
    
    * feat(Choice & Program): Make Choice & Program component responsive
    
    * Refactor: Separate main and auth routes into distinct layouts
    
    * feat(login): implement login page with animated images
    
    * feat(login): create login page with animated images
    
    * fix: fix merge conflict in signup page
    
    * feat: create forget-password page
    
    * feat: create verify-code page
    
    * feat: create verify-code page
    
    ---------
    
    Co-authored-by: Gbadamosi Sofiyat <sofiyatgbadamosi@gmail.com>

--- src/pages/auth/verify-code.jsx
+++ src/pages/auth/verify-code.jsx
@@ -1,7 +1,189 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import topLeft from "../../images/topLeft.png";
+import bottomLeft from "../../images/bottomLeft.png";
+import topRight from "../../images/topRight.png";
+import bottomRight from "../../images/bottomRight.png";
+import greenLogo from "../../images/greenLogo.png";
+import whiteLogo from "../../images/whiteLogo.png";
+import { IoCloseSharp } from "react-icons/io5";
+import { IoIosArrowBack } from "react-icons/io";
 
 const VerifyCode = () => {
-  return <div>verify-code</div>;
+  const [errors, setErrors] = useState({});
+  const [formData, setFormData] = useState({
+    code: "",
+  });
+  const navigate = useNavigate();
+
+  const validateForm = () => {
+    let newErrors = {};
+    if (!formData.code) {
+      newErrors.code = "Code is required";
+    } else if (!/^\d+$/.test(formData.code)) {
+      newErrors.code = "Code must contain only numbers";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleClose = () => {
+    navigate("/");
+  };
+  const toLogin = () => {
+    navigate("/login");
+  };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      navigate("/resetpassword");
+    }
+  };
+  return (
+    <div className="relative   flex md:flex-row flex-col overflow-hidden">
+      {/* Green Background (Desktop) */}
+      <div className="bg-[#00A665] h-screen w-[40%] hidden md:block relative">
+        <div className="mr-12 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex space-x-4">
+          <img src={whiteLogo} alt="" />
+        </div>
+        {/* Motion Images */}
+        <motion.img
+          src={topLeft}
+          alt="topLeft"
+          className="absolute -top-8 left-0 w-[250px]"
+          animate={{
+            y: [0, -20, 0], // Moves up and down
+          }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            repeatType: "reverse",
+            ease: "easeInOut",
+          }}
+        />
+        <motion.img
+          src={bottomLeft}
+          alt="bottomLeft"
+          className="absolute bottom-0 left-0"
+          animate={{
+            y: [0, 20, 0], // Moves down and up
+          }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            repeatType: "reverse",
+            ease: "easeInOut",
+          }}
+        />
+
+        <motion.img
+          src={topRight}
+          alt="topRight"
+          className="absolute -top-8 right-0"
+          animate={{
+            y: [0, -20, 0], // Moves down and up
+          }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            repeatType: "reverse",
+            ease: "easeInOut",
+          }}
+        />
+        <motion.img
+          src={bottomRight}
+          alt="bottomRight"
+          className="absolute bottom-0 right-0"
+          animate={{
+            y: [0, 20, 0], // Moves down and up
+          }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            repeatType: "reverse",
+            ease: "easeInOut",
+          }}
+        />
+      </div>
+      {/* form */}
+      <div className="w-full md:w-[60%] p-8 md:px-2 flex flex-col justify-center items-center">
+        <div className="w-full p-16">
+          <div className="flex justify-between items-center md:float-right">
+            <img src={greenLogo} alt="" className="block md:hidden" />
+            <button onClick={handleClose} className="">
+              <IoCloseSharp className="w-8 h-8 p-1 border border-[#757373]  text-[#757373] rounded-full" />
+            </button>
+          </div>
+
+          <div className=" mt-16">
+            <div className="md:flex md:justify-center md:items-center hidden">
+              <button
+                onClick={toLogin}
+                className="cursor pointer flex items-center space-x-2"
+              >
+                <IoIosArrowBack className="text-[#1E1E1E]" />
+                <span className="text-[#1E1E1E] text-[16px]">
+                  Back to Login{" "}
+                </span>
+              </button>
+            </div>
+
+            <h2 className="text-center text-[24px] md:text-[35px] text-[#1E1E1E] font-bold mb-2">
+              Verify Code
+            </h2>
+            <p className="text-center text-[14px] md:text-[16px] text-[#6D737A] mb-6">
+              An authentication code has been sent to your email.
+            </p>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label
+                className="block text-[#1E1E1E] text-[14px] md:text-[16px]"
+                htmlFor="code"
+              >
+                Code
+              </label>
+              <input
+                className="mt-2 w-full p-3 border border-[#1E1E1E] rounded-[15px] focus:outline-none focus:ring focus:ring-[#1E1E1E]"
+                id="code"
+                type="text"
+                placeholder="Enter Code"
+                name="code"
+                value={formData.code}
+                onChange={handleChange}
+              />
+              {errors.code && (
+                <p className="text-red-500 text-sm">{errors.code}</p>
+              )}
+            </div>
+            <button
+              className="mt-8 bg-[#009E65]text-white py-6 md:px-6 md:py-4 text-white text-[14px] md:text-[16px] hover:text-[#009E65] hover:font-medium bg-[#009E65] hover:bg-white hover:border-2 hover:border-[#009E65] w-full   rounded-[15px] cursor-pointer"
+              type="submit"
+            >
+              Verify
+            </button>
+          </form>
+          <div className="text-center mt-10">
+            <p className="text-[14px] md:text-[16px] text-[#1E1E1E] font-medium">
+              Didn't receive a code?{" "}
+              <Link
+                to=""
+                className="text-[#00A665] font-medium hover:underline"
+              >
+                Resend
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VerifyCode;
