91d1115ab40c93b34495b05cd54bc66281924643
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2025-03-31 05:34:49 +0100

    Development (#22)
    * feat(homepage): Restructure layout to align with updated design specifications
    
    * feat(hero_component): implement animated text loop, input field and floating icons
    
    * fix(hero_component): correctly align profile images
    
    * feat(statistics_component): Implement animated stats section that triggers on viewport entry
    
    * feat(statistics_component): Implement animated stat section that triggers on viewport entry
    
    * feat(About_component): add the about section with navigation to the about page
    
    * feat: Add About component with in-view text animation and menu hover effect
    
    * fix(Statistics_component) :Stepwise Count-Up with Smooth Upward Animation
    
    * fix: resolve merge conflict in About component
    
    * Add program component with pricing and signup link
    
    * feat(auth): Add authentication pages for enrollment, signup, login, and password management
    
    * feat(Choice_component): add choice section with icons, title and description
    
    * feat(about_page): create about page, including leadership team, core values, and impact sections
    
    * fix(modal): Adjust overlay opacity to allow background content to be more
    
    * feat(program_page): Create hero section with text and image layout
    
    * fix(program_page): Adjust thinGreen1 & thinGreen2 positioning to ensure it stays behind program_hero
    
    * feat(mentorship_page_: add mentorship page with hero and mentors section
    
    * Updated header, footer and partners section (#9)
    
    * feat: create FAQs page with framer motion for smooth animations
    
    * feat: add contact page with form submission
    
    * feat: add enroll page with form validation and dynamic image carousel
    
    * feat(Hero): Make Hero component responsive
    
    * feat(About & Statistics): Make About & Statistics component responsive
    
    * feat(Choice & Program): Make Choice & Program component responsive
    
    * Refactor: Separate main and auth routes into distinct layouts
    
    * feat(login): implement login page with animated images
    
    * feat(login): create login page with animated images
    
    * fix: fix merge conflict in signup page
    
    * feat: create forget-password page
    
    * feat: create verify-code page
    
    * feat: create verify-code page
    
    * feat: create resetpassword page
    
    ---------
    
    Co-authored-by: Gbadamosi Sofiyat <sofiyatgbadamosi@gmail.com>

--- src/pages/auth/signup.jsx
+++ src/pages/auth/signup.jsx
@@ -5,7 +5,6 @@ import topLeft from "../../images/topLeft.png";
 import bottomLeft from "../../images/bottomLeft.png";
 import topRight from "../../images/topRight.png";
 import bottomRight from "../../images/bottomRight.png";
-
 import logo from "../..//images/logo.png";
 import whiteLogo from "../../images/whiteLogo.png";
 import greenLogo from "../../images/greenLogo.png";
@@ -76,8 +75,6 @@ const Signup = () => {
     }, 1000); // Simulate a 1-second submission delay
   };
 
-  // Framer Motion Animations
-
   useEffect(() => {
     if (isSubmitted) {
       const redirectTimeout = setTimeout(() => {
@@ -121,12 +118,11 @@ const Signup = () => {
   }
 
   return (
-    <div className="relative   flex md:flex-row flex-col overflow-hidden">
+    <div className="relative h-screen  flex md:flex-row flex-col md:overflow-hidden">
       {/* Green Background (Desktop) */}
 
       <div className="bg-[#00A665] h-screen w-[40%] hidden md:block relative">
         <div className="mr-12 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex space-x-4">
-
           <img src={whiteLogo} alt="" />
         </div>
         {/* Motion Images */}
@@ -325,12 +321,9 @@ const Signup = () => {
           <div className="mt-6 text-center ">
             <p className="text-[14px] md:text-[16px] text-[#1E1E1E] font-medium">
               Already have an Account?{" "}
-
               <Link
                 to="/login"
                 className="text-[#00A665] font-medium hover:underline"
-
-
               >
                 Login
               </Link>
