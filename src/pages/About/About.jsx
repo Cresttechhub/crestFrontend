058443ced868aaa114375afd6bce7f9cf6429a5c
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2025-03-31 20:41:23 +0100

    Development (#23)
    * feat(homepage): Restructure layout to align with updated design specifications
    
    * feat(hero_component): implement animated text loop, input field and floating icons
    
    * fix(hero_component): correctly align profile images
    
    * feat(statistics_component): Implement animated stats section that triggers on viewport entry
    
    * feat(statistics_component): Implement animated stat section that triggers on viewport entry
    
    * feat(About_component): add the about section with navigation to the about page
    
    * feat: Add About component with in-view text animation and menu hover effect
    
    * fix(Statistics_component) :Stepwise Count-Up with Smooth Upward Animation
    
    * fix: resolve merge conflict in About component
    
    * Add program component with pricing and signup link
    
    * feat(auth): Add authentication pages for enrollment, signup, login, and password management
    
    * feat(Choice_component): add choice section with icons, title and description
    
    * feat(about_page): create about page, including leadership team, core values, and impact sections
    
    * fix(modal): Adjust overlay opacity to allow background content to be more
    
    * feat(program_page): Create hero section with text and image layout
    
    * fix(program_page): Adjust thinGreen1 & thinGreen2 positioning to ensure it stays behind program_hero
    
    * feat(mentorship_page_: add mentorship page with hero and mentors section
    
    * Updated header, footer and partners section (#9)
    
    * feat: create FAQs page with framer motion for smooth animations
    
    * feat: add contact page with form submission
    
    * feat: add enroll page with form validation and dynamic image carousel
    
    * feat(Hero): Make Hero component responsive
    
    * feat(About & Statistics): Make About & Statistics component responsive
    
    * feat(Choice & Program): Make Choice & Program component responsive
    
    * Refactor: Separate main and auth routes into distinct layouts
    
    * feat(login): implement login page with animated images
    
    * feat(login): create login page with animated images
    
    * fix: fix merge conflict in signup page
    
    * feat: create forget-password page
    
    * feat: create verify-code page
    
    * feat: create verify-code page
    
    * feat: create resetpassword page
    
    * feat: implement responsiveness in the About page
    
    ---------
    
    Co-authored-by: Gbadamosi Sofiyat <sofiyatgbadamosi@gmail.com>

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -85,11 +85,11 @@ const leadershipTeam = [
 ];
 const About = () => {
   return (
-    <div className="mt-36 ">
+    <div className="mt-28 md:mt-36 ">
       {/* about */}
-      <div className="px-16 py-6">
+      <div className="md:px-16 md:py-6 p-8">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div className="w-full">
+          <div className="hidden md:block w-full">
             <img
               src={aboutImg}
               alt="About Crest Tech Hub"
@@ -97,10 +97,10 @@ const About = () => {
             />
           </div>
           <div className="w-full flex flex-col justify-start mt-6">
-            <h1 className="text-[30px] text-[#1E1E1E] font-bold mb-8">
+            <h1 className="text-[18px] md:text-[30px] text-[#1E1E1E] font-bold mb-8">
               About Crest Tech Hub
             </h1>
-            <div className="text-[#6D737A] text-[16px] leading-[36px]">
+            <div className="text-[#6D737A] text-[14px] md:text-[16px] leading-[36px]">
               <p>
                 Founded in 2020, Crest Tech Hub emerged from a vision to bridge
                 the gap between traditional education and the rapidly evolving
@@ -117,27 +117,39 @@ const About = () => {
               </p>
             </div>
           </div>
+          <div className="block w-full md:hidden">
+            <img
+              src={aboutImg}
+              alt="About Crest Tech Hub"
+              className="w-[667px] h-[447px] rounded-[25px] object-cover"
+            />
+          </div>
         </div>
       </div>
 
       {/* mission & vision */}
-      <div className="mt-10 px-30 py-6">
-        <div className="flex justify-between">
+      <div className="-mt-6 md:mt-10 md:px-30 md:py-6 p-8">
+        <div className="flex flex-col md:flex-row justify-between">
           <div className="w-full">
-            <h1 className="text-[#1E1E1E] text-[20px] font-bold mb-8">
+            <h1 className="text-[#1E1E1E] text-[18px] md:text-[20px] font-bold mb-8">
               Our Mission
             </h1>
-            <p className="text-[#6D737A] text-[16px] leading-[30px] max-w-[537px]">
+            <p className="text-[#6D737A] text-[14px] md:text-[16px] leading-[30px] max-w-[537px]">
               To provide accessible, high quality tech education that empowers
               individuals to build successful tech careers in technology.
             </p>
           </div>
-          <div className="h-40 border border-[#6D737A] opacity-20 mx-8" />
-          <div className="w-full">
-            <h1 className="text-[#1E1E1E] text-[20px] font-bold mb-8">
+          <div className="block md:hidden w-full my-8">
+            <div className="border-b border-[#6D737A] opacity-20" />
+          </div>
+          <div className="hidden md:flex items-center justify-center">
+            <div className="border-r border-[#6D737A] h-40  opacity-20" />
+          </div>
+          <div className="w-full md:ml-16">
+            <h1 className="text-[#1E1E1E] text-[18px] md:text-[20px] font-bold mb-8">
               Our Vision
             </h1>
-            <p className="text-[#6D737A] text-[16px] leading-[30px] max-w-[537px]">
+            <p className="text-[#6D737A] text-[14px] md:text-[16px] leading-[30px] max-w-[537px]">
               To be the leading platform for tech education, creating the next
               generation of tech leaders.
             </p>
@@ -145,20 +157,20 @@ const About = () => {
         </div>
       </div>
       {/* impact */}
-      <div className="mt-10 px-30 py-6">
-        <h2 className="text-[30px] text-[#1E1E1E] font-bold  text-center mb-12">
+      <div className="-mt-6 md:mt-10 md:px-30 md:py-6 p-8">
+        <h2 className="text-[18px] md:text-[20px] text-[#1E1E1E] font-bold  md:text-center mb-12">
           Our impact
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-16">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-16">
           {impacts.map((impact, index) => (
             <div key={index} className="flex flex-col items-start relative">
               <div className="px-4 py-2 bg-[#E4FFF2] border border-[#00A665] rounded-[10px] mb-4">
                 {impact.icon}
               </div>
-              <h3 className="text-[34px] font-bold  text-[#00A665]">
+              <h3 className="text-[18px] md:text-[34px] font-bold  text-[#00A665]">
                 {impact.number}
               </h3>
-              <p className="text-[#6D737A] mt-2 text-[16px]">
+              <p className="text-[#6D737A] mt-2 text-[14px] md:text-[16px]">
                 {impact.description}
               </p>
               {index < values.length - 1 && (
@@ -169,20 +181,20 @@ const About = () => {
         </div>
       </div>
       {/* core values */}
-      <div className="mt-10 px-30 py-6">
-        <h2 className="text-[30px] text-[#1E1E1E] font-bold  text-center mb-12">
+      <div className="md:mt-10 md:px-30 md:py-6 p-8">
+        <h2 className="text-[18px] md:text-[20px] text-[#1E1E1E] font-bold  md:text-center mb-12">
           Our Core Values
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-16">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-16">
           {values.map((value, index) => (
             <div key={index} className="flex flex-col items-start relative">
               <div className="px-4 py-2 bg-[#E4FFF2] border border-[#00A665] rounded-[10px] mb-4">
                 {value.icon}
               </div>
-              <h3 className="text-[34px] font-bold  text-[#00A665]">
+              <h3 className="text-[16px] md:text-[34px] font-bold  text-[#00A665]">
                 {value.title}
               </h3>
-              <p className="text-[#6D737A] mt-2 text-[16px] max-w-[342px]">
+              <p className="text-[14px] md:text-[16px] text-[#6D737A] mt-2  max-w-[342px]">
                 {value.description}
               </p>
               {index < values.length - 1 && (
@@ -193,22 +205,24 @@ const About = () => {
         </div>
       </div>
       {/* leadership team */}
-      <div className="mt-10 px-16 py-6">
-        <h1 className="text-[#1E1E1E] text-[30px] font-bold mb-8 text-center">
+      <div className="md:mt-10 md:px-16 md:py-6 p-8">
+        <h1 className="text-[#1E1E1E] text-[18px] md:text-[30px] font-bold mb-8 md:text-center">
           Our Leadership Team
         </h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-4 gap-8">
           {leadershipTeam.map((leader, index) => (
             <div key={index} className="flex flex-col space-y-3 items-center">
               <img
                 src={leader.image}
                 alt={leader.name}
-                className="w-[261px] h-[247px] rounded-[20px] bg-[#F6F6F8] object-cover"
+                className="w-full md:w-[261px] md:h-[247px] rounded-[20px] bg-[#F6F6F8] object-cover"
               />
-              <h3 className="text-[20px] font-bold text-[#1E1E1E]">
+              <h3 className="text-[18px] md:text-[20px] font-bold text-[#1E1E1E]">
                 {leader.name}
               </h3>
-              <p className="text-[#6D737A] text-[16px]">{leader.position}</p>
+              <p className="text-[#6D737A] text-[14px] md:text-[16px]">
+                {leader.position}
+              </p>
               <div className="flex space-x-3 text-[#00A665]">
                 <a
                   href={leader.facebook}
