edf92d85ec02cdd16b6768d857e5548e53486d89
EniolaCodes <adebimpeniola@gmail.com>
2026-03-03 11:49:34 +0100

    fix: add responsiveness to Choice section

--- src/components/Choice/Choice.jsx
+++ src/components/Choice/Choice.jsx
@@ -26,33 +26,31 @@ const features = [
 const Choice = () => {
   return (
     <section className="md:py-10 md:px-24 p-8">
-      <div className="">
-        <h2 className="text-[18px] md:text-[30px] text-[#1E1E1E] font-bold  text-center mb-12">
-          Why Choose CrestTech Hub?
-        </h2>
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
-          {features.map((feature, index) => (
-            <div key={index} className="flex flex-col items-start relative">
-              <div className="px-4 py-2 bg-[#E4FFF2] border border-[#00A665] rounded-[10px] mb-4">
-                {feature.icon}
-              </div>
-              <h3 className="text-[16px] md:text-lg font-bold text-[#1E1E1E] md:break-words flex flex-col">
-                {feature.title}
-              </h3>
-              <p className="text-[#6D737A] mt-2 text-[14px] md:text-[16px] max-w-[200px]">
-                {feature.description}
-              </p>
-
-              {index < features.length - 1 && (
-                <div
-                  className={`border-r border-[#6D737A] opacity-20 absolute top-0 bottom-0 right-0 ${
-                    index % 2 === 0 ? "block" : "hidden md:block"
-                  }`}
-                ></div>
-              )}
+      <h2 className="text-[18px] md:text-[30px] text-[#1E1E1E] font-bold  text-center mb-12">
+        Why Choose CrestTech Hub?
+      </h2>
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+        {features.map((feature, index) => (
+          <div key={index} className="flex flex-col items-start relative">
+            <div className="px-4 py-2 bg-[#E4FFF2] border border-[#00A665] rounded-[10px] mb-4">
+              {feature.icon}
             </div>
-          ))}
-        </div>
+            <h3 className="text-[16px] md:text-lg font-bold text-[#1E1E1E] md:break-words flex flex-col">
+              {feature.title}
+            </h3>
+            <p className="text-[#6D737A] mt-2 text-[14px] md:text-[16px] max-w-[200px]">
+              {feature.description}
+            </p>
+
+            {index < features.length - 1 && (
+              <div
+                className={`border-r border-[#6D737A] opacity-20 absolute top-0 bottom-0 right-0 ${
+                  index % 2 === 0 ? "block" : "hidden md:block"
+                }`}
+              ></div>
+            )}
+          </div>
+        ))}
       </div>
     </section>
   );
